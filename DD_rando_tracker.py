2da79440bac3bdb41dcfbddeefa8bacb7ed828fd
SpR3AD1 <joernbardoauscl@yahoo.de>
2024-04-09 11:16:01 +0200

    version fix

--- DD_rando_tracker.py
+++ DD_rando_tracker.py
@@ -6,6 +6,7 @@ from os import makedirs
 from PIL import Image, ImageTk
 import sys
 from matplotlib.colors import is_color_like
+from tkinter import ttk
 
 show = True
 exitFlag = False
@@ -14,6 +15,7 @@ bg_color = "#101010"
 fg_color = "#ffffff"
 window_x = 288
 window_y = 657
+accessable = 1
 
 def button_found_click():
     global show
@@ -29,62 +31,75 @@ def insert_text(x):
             listbox.delete(0,tk.END)
         listbox.insert(0,*x)
     
-def remove_missing(Locations, Items, Time):
+def read_file():
+    Locations = []
     Items_found = []
     Time_found = []
     Locations_missing = []
-    Locations_found = []
-    seeds_found = 0
     souls_found = 0
+    seeds_found = 0
     vitality_found = 0
     magic_found = 0
-    for i in range(len(Time)):
-        if not Time[i] == '':
-            match Items[i]:
-                case '100 Souls':
-                     souls_found += 1
-                case 'Life Seed':
-                     seeds_found += 1
-                case 'Vitality Shard':
-                     vitality_found += 1
-                case 'Magic Shard':
-                     magic_found += 1
-                case _:
-                    Items_found.append(Items[i])
-                    Time_found.append(Time[i])
-            Locations_found.append(Locations[i])
-        else:
-            Locations_missing.append(Locations[i])
-    Items_found = [x for _, x in sorted(zip(Time_found, Items_found), reverse=True)]
-    Time_found = sorted(Time_found, reverse=True)
-    Locations_missing = [x for x in Locations_missing if x not in Locations_found]
-    return Items_found, Time_found, Locations_missing, souls_found, seeds_found, vitality_found, magic_found
-    
-def read_file():
-    Locations = []
-    Items = []
-    Time = []
     souls_count = 0
     seeds_count = 0
     vitality_count = 0
     magic_count = 0
+    pink_key_found = 0
+    green_key_found = 0
+    yellow_key_found = 0
+    pink_key_count = 0
+    green_key_count = 0
+    yellow_key_count = 0
     filepath = getenv('LocalAppData') + 'Low\Acid Nerve\DeathsDoor\SAVEDATA\\' + filepath_var.get() + '-ItemChanger.json'
 
     if path.isfile(filepath):
         f = open(filepath, 'r')
         content = f.read()
         if len(content)>0:
+            # found items
             for x in content.split('"LocationName"'):
                 y = x.split('"')
                 if not y[1] == 'Placements' and len(y)>5:
                     Locations.append(y[1])
-                    Items.append(y[5])
+                    # Items_found.append(y[5])
+                    match y[5]:
+                        case '100 Souls':
+                            souls_found += 1
+                        case 'Life Seed':
+                            seeds_found += 1
+                        case 'Vitality Shard':
+                            vitality_found += 1
+                        case 'Magic Shard':
+                            magic_found += 1
+                        case 'Pink Key':
+                            pink_key_found += 1
+                        case 'Green Key':
+                            green_key_found += 1
+                        case 'Yellow Key':
+                            yellow_key_found += 1
+                        case _:
+                            Items_found.append(y[5])
                     if len(y)>12:
                         z = y[12].split(':')
-                        Time.append(float(z[1][:-3]))
+                        Time_found.append(float(z[1][:-3]))
+            # missing items
+            x = content.split('"TrackerLog"')
+            loc = True
+            insert = False
+            go = False
+            for y in x[0].split('"'):
+                if go and y not in [',',':','},']:
+                    if loc:
+                        if not y in Locations:
+                            Locations.append(y)
+                            Locations_missing.append(y)
+                            insert = True
+                        loc = False
                     else:
-                        Time.append('')
-                        match y[5]:
+                        if insert:
+                            insert = False
+                        loc = True
+                        match y:
                             case '100 Souls':
                                  souls_count += 1
                             case 'Life Seed':
@@ -93,8 +108,28 @@ def read_file():
                                  vitality_count += 1
                             case 'Magic Shard':
                                  magic_count += 1
+                            case 'Pink Key':
+                                pink_key_count += 1
+                            case 'Green Key':
+                                green_key_count += 1
+                            case 'Yellow Key':
+                                yellow_key_count += 1
+                else:
+                    if ':{' in y:
+                        go = True
         f.close()
-    return Locations, Items, Time, souls_count, seeds_count, vitality_count, magic_count
+        Items_found = [x for _, x in sorted(zip(Time_found, Items_found), reverse=True)]
+        
+    filepath = getenv('LocalAppData') + 'Low\Acid Nerve\DeathsDoor\SAVEDATA\\Randomizer Helper Log.txt'
+    Locations_accessable = []
+    if path.isfile(filepath):
+        f = open(filepath, 'r')
+        for line in f.readlines():
+            if not ':' in line and line.strip():
+                if not line in Locations_accessable:
+                    Locations_accessable.append(line)
+        
+    return Items_found, Time_found, Locations, Locations_missing, Locations_accessable, souls_found, seeds_found, vitality_found, magic_found, souls_count, seeds_count, vitality_count, magic_count, pink_key_found, yellow_key_found, green_key_found, pink_key_count, yellow_key_count, green_key_count
     
 def filter_list(x):
     lookfor = entry.get()
@@ -135,8 +170,7 @@ def mainloop():
         err = False
         msgbox.showinfo(title='Error', message="Error when using colors. Reverting to default.")
     if not exitFlag:
-        Locations, Items, Time, souls_count, seeds_count, vitality_count, magic_count = read_file()
-        Items_found, Time_found, Locations_missing, souls_found, seeds_found, vitality_found, magic_found = remove_missing(Locations, Items, Time)
+        Items_found, Time_found, Locations, Locations_missing, Locations_accessable, souls_found, seeds_found, vitality_found, magic_found, souls_count, seeds_count, vitality_count, magic_count, pink_key_found, yellow_key_found, green_key_found, pink_key_count, yellow_key_count, green_key_count = read_file()
         if "Giant Soul of The Urn Witch" in Items_found:
             canvas.itemconfig(grandma,image=grandma_found)
         else:
@@ -149,6 +183,7 @@ def mainloop():
             canvas.itemconfig(betty,image=betty_found)
         else:
             canvas.itemconfig(betty,image=betty_missing)
+        '''
         if "Discarded Umbrella" in Items_found:
             canvas.itemconfig(umbrella,image=umbrella_found)
         else:
@@ -165,6 +200,7 @@ def mainloop():
             canvas.itemconfig(greatsword,image=greatsword_found)
         else:
             canvas.itemconfig(greatsword,image=greatsword_missing)
+        '''
         if "Arrow Level 2" in Items_found:
             canvas.itemconfig(arrow,image=arrow_found2)
         elif "Arrow" in Items_found or True:
@@ -190,10 +226,18 @@ def mainloop():
             canvas.itemconfig(hookshot,image=hookshot_found)
         else:
             canvas.itemconfig(hookshot,image=hookshot_missing)
+        canvas.itemconfig(pink_key_text,text=f'{pink_key_found} / {pink_key_count}', fill=fg_color)
+        canvas.itemconfig(yellow_key_text,text=f'{yellow_key_found} / {yellow_key_count}', fill=fg_color)
+        canvas.itemconfig(green_key_text,text=f'{green_key_found} / {green_key_count}', fill=fg_color)
         canvas.itemconfig(souls_text,text=f'{souls_found} / {souls_count}', fill=fg_color)
         canvas.itemconfig(seeds_text,text=f'{seeds_found} / {seeds_count}', fill=fg_color)
         canvas.itemconfig(vitality_text,text=f'{vitality_found} / {vitality_count}', fill=fg_color)
         canvas.itemconfig(magic_text,text=f'{magic_found} / {magic_count}', fill=fg_color)
+        
+        if accessable:
+            accessable_text.config(text=f'accessable locations: {len(Locations_accessable)} / {len(Locations_missing)} / {len(Locations)}')
+        else:
+            accessable_text.config(text=f'unchecked locations: {len(Locations_missing)} / {len(Locations)}')
 
         filterlist = ["Giant Soul of The Urn Witch", "Giant Soul of The Frog King", "Giant Soul of Betty", "Discarded Umbrella", "Rogue Daggers", "Thunder Hammer", "Reaper's Greatsword", "Reaper's Greatsword", "Arrow Level 2", "Fire", "Fire Level 2", "Bomb", "Bomb Level 2", "Hookshot", "Hookshot Level 2"]
         Items_found = [x for x in Items_found if x not in filterlist]
@@ -201,7 +245,10 @@ def mainloop():
         if show:
             insert_text(filter_list(Items_found))
         else:
-            insert_text(filter_list(Locations_missing))
+            if accessable:
+                insert_text(filter_list(Locations_accessable))
+            else:
+                insert_text(filter_list(Locations_missing))
 
         window.after(200, mainloop)
 
@@ -225,7 +272,7 @@ def mainloop():
             
             makedirs(path.dirname(configpath), exist_ok=True)
             f = open(configpath, "w")
-            f.write(f"bg_color = {bg_color}\nfg_color = {fg_color}\nwidth = {window_x}\nheight = {window_y}")
+            f.write(f"bg_color = {bg_color}\nfg_color = {fg_color}\nwidth = {window_x}\nheight = {window_y}\naccessable = {accessable}")
             f.close()
             
             window.configure(bg=bg_color)
@@ -243,6 +290,8 @@ def open_settings():
     global fg_color
     global window_x
     global window_y
+    global access
+    global accessable
     
     def get_settings():
         windowx.delete(0,tk.END)
@@ -265,6 +314,8 @@ def open_settings():
         global fg_color
         global window_x
         global window_y
+        global access
+        global accessable
         if not is_color_like(bgcolor.get()):
             msgbox.showinfo(title='Wrong input', message="Backround-color: please enter a valid color or hex-code.")
         elif not is_color_like(fgcolor.get()):
@@ -274,12 +325,13 @@ def open_settings():
             fg_color = fgcolor.get()
             window_x = windowx.get()
             window_y = windowy.get()
+            accessable = access.get()
             
             configpath = getenv('LocalAppData') + 'Low\Acid Nerve\DeathsDoor\DD_rando_tracker\Config.cfg'
             
             makedirs(path.dirname(configpath), exist_ok=True)
             f = open(configpath, "w")
-            f.write(f"bg_color = {bg_color}\nfg_color = {fg_color}\nwidth = {window_x}\nheight = {window_y}")
+            f.write(f"bg_color = {bg_color}\nfg_color = {fg_color}\nwidth = {window_x}\nheight = {window_y}\naccessable = {accessable}")
             f.close()
             
             window.geometry(f"{window_x}x{window_y}")
@@ -289,7 +341,7 @@ def open_settings():
     newWindow = tk.Toplevel(window)
     newWindow.configure(bg=bg_color)
     newWindow.title("Settings")
-    newWindow.geometry("200x205")
+    newWindow.geometry("200x255")
 
     tk.Label(newWindow, text ="Background-color:", bg=bg_color, fg=fg_color).pack()
     bgcolor = tk.Entry(newWindow, bg=bg_color, fg=fg_color, textvariable = tk.StringVar(newWindow, value=bg_color))
@@ -303,6 +355,9 @@ def open_settings():
     tk.Label(newWindow, text ="window-height:", bg=bg_color, fg=fg_color).pack()
     windowy = tk.Entry(newWindow, bg=bg_color, fg=fg_color, textvariable = tk.StringVar(newWindow, value=window_y))
     windowy.pack()
+    
+    access = tk.IntVar(value=accessable)
+    checkbutton = tk.Checkbutton(newWindow, bg=bg_color, fg=fg_color, selectcolor=bg_color, text="only accessable locations", variable=access, onvalue=1, offvalue=0).pack()
     
     buttonframe = tk.Frame(newWindow, bg=bg_color)
     buttonframe.pack(pady=(10, 0))
@@ -322,10 +377,16 @@ if path.isfile(configpath):
     f = open(configpath, 'r')
     content = f.read()
     content = content.split('\n')
-    bg_color = content[0].split(" = ")[1]
-    fg_color = content[1].split(" = ")[1]
-    window_x = int(content[2].split(" = ")[1])
-    window_y = int(content[3].split(" = ")[1])
+    if len(content)>0:
+        bg_color = content[0].split(" = ")[1]
+    if len(content)>1:
+        fg_color = content[1].split(" = ")[1]
+    if len(content)>2:
+        window_x = int(content[2].split(" = ")[1])
+    if len(content)>3:
+        window_y = int(content[3].split(" = ")[1])
+    if len(content)>4:
+        accessable = int(content[4].split(" = ")[1])
     f.close()
 
 window = tk.Tk()
@@ -335,7 +396,7 @@ window.title("Death's Door Randomizer Tracker")
 
 window.protocol("WM_DELETE_WINDOW", on_closing)
 
-canvas = ResizingCanvas(width=window_x, height=260, bg=bg_color, highlightthickness=0)
+canvas = ResizingCanvas(width=window_x, height=235, bg=bg_color, highlightthickness=0)
 canvas.pack(fill="x")
 
 bundle_dir = getattr(sys, '_MEIPASS', path.abspath(path.dirname(__file__)))
@@ -399,7 +460,25 @@ hookshot_found = ImageTk.PhotoImage(img.resize((50,50), Image.LANCZOS))
 img= Image.open(path.join(bundle_dir, "images/Hookshot2.png"))
 hookshot_found2 = ImageTk.PhotoImage(img.resize((50,50), Image.LANCZOS))
 
+img= Image.open(path.join(bundle_dir, "images/PinkKey.png"))
+pink_key_img = ImageTk.PhotoImage(img.resize((30,30), Image.LANCZOS))
+pink_key = canvas.create_image(window_x/(288/10),125,image=pink_key_img, anchor="nw")
 
+pink_key_text = canvas.create_text(window_x/(288/43), 128, text='X / Y', anchor="nw", fill=fg_color, font=('Helvetica 16 bold'))
+
+img= Image.open(path.join(bundle_dir, "images/YellowKey.png"))
+yellow_key_img = ImageTk.PhotoImage(img.resize((30,30), Image.LANCZOS))
+yellow_key = canvas.create_image(window_x/(288/105),125,image=yellow_key_img, anchor="nw")
+
+yellow_key_text = canvas.create_text(window_x/(288/138), 128, text='X / Y', anchor="nw", fill=fg_color, font=('Helvetica 16 bold'))
+
+img= Image.open(path.join(bundle_dir, "images/GreenKey.png"))
+green_key_img = ImageTk.PhotoImage(img.resize((30,30), Image.LANCZOS))
+green_key = canvas.create_image(window_x/(288/205),125,image=green_key_img, anchor="nw")
+
+green_key_text = canvas.create_text(window_x/(288/238), 128, text='X / Y', anchor="nw", fill=fg_color, font=('Helvetica 16 bold'))
+
+'''
 img= Image.open(path.join(bundle_dir, "images/Umbrella_missing.png"))
 umbrella_missing = ImageTk.PhotoImage(img.resize((50,50), Image.LANCZOS))
 umbrella = canvas.create_image(window_x/(288/37.6),120,image=umbrella_missing, anchor="n")
@@ -423,32 +502,35 @@ greatsword_missing = ImageTk.PhotoImage(img.resize((50,50), Image.LANCZOS))
 greatsword = canvas.create_image(window_x/(288/250.4),120,image=greatsword_missing, anchor="n")
 img= Image.open(path.join(bundle_dir, "images/Greatsword.png"))
 greatsword_found = ImageTk.PhotoImage(img.resize((50,50), Image.LANCZOS))
-
+'''
 
 img= Image.open(path.join(bundle_dir, "images/Seed.png"))
-seed_img = ImageTk.PhotoImage(img.resize((40,40), Image.LANCZOS))
-seed = canvas.create_image(window_x/(288/5),175,image=seed_img, anchor="nw")
+seed_img = ImageTk.PhotoImage(img.resize((30,30), Image.LANCZOS))
+seed = canvas.create_image(window_x/(288/5),165,image=seed_img, anchor="nw")
 
-seeds_text = canvas.create_text(window_x/(288/45), 180, text='X / Y', anchor="nw", fill=fg_color, font=('Helvetica 18 bold'))
+seeds_text = canvas.create_text(window_x/(288/45), 168, text='X / Y', anchor="nw", fill=fg_color, font=('Helvetica 16 bold'))
 
 img= Image.open(path.join(bundle_dir, "images/Soul.png"))
-soul_img = ImageTk.PhotoImage(img.resize((40,40), Image.LANCZOS))
-soul = canvas.create_image(window_x/(288/283),175,image=soul_img, anchor="ne")
+soul_img = ImageTk.PhotoImage(img.resize((30,30), Image.LANCZOS))
+soul = canvas.create_image(window_x/(288/283),165,image=soul_img, anchor="ne")
 
-souls_text = canvas.create_text(window_x/(288/243), 180, text='X / Y', anchor="ne", fill=fg_color, font=('Helvetica 18 bold'))
+souls_text = canvas.create_text(window_x/(288/243), 168, text='X / Y', anchor="ne", fill=fg_color, font=('Helvetica 16 bold'))
 
 
 img= Image.open(path.join(bundle_dir, "images/VitalityShard.png"))
-vitality_img = ImageTk.PhotoImage(img.resize((40,40), Image.LANCZOS))
-vitality = canvas.create_image(window_x/(288/5),220,image=vitality_img, anchor="nw")
+vitality_img = ImageTk.PhotoImage(img.resize((30,30), Image.LANCZOS))
+vitality = canvas.create_image(window_x/(288/5),200,image=vitality_img, anchor="nw")
 
-vitality_text = canvas.create_text(window_x/(288/45), 225, text='X / Y', anchor="nw", fill=fg_color, font=('Helvetica 18 bold'))
+vitality_text = canvas.create_text(window_x/(288/45), 203, text='X / Y', anchor="nw", fill=fg_color, font=('Helvetica 16 bold'))
 
 img= Image.open(path.join(bundle_dir, "images/MagicShard.png"))
-magic_img = ImageTk.PhotoImage(img.resize((40,40), Image.LANCZOS))
-magic = canvas.create_image(window_x/(288/283),220,image=magic_img, anchor="ne")
+magic_img = ImageTk.PhotoImage(img.resize((30,30), Image.LANCZOS))
+magic = canvas.create_image(window_x/(288/283),200,image=magic_img, anchor="ne")
 
-magic_text = canvas.create_text(window_x/(288/243), 225, text='X / Y', anchor="ne", fill=fg_color, font=('Helvetica 18 bold'))
+magic_text = canvas.create_text(window_x/(288/243), 203, text='X / Y', anchor="ne", fill=fg_color, font=('Helvetica 16 bold'))
+
+accessable_text = tk.Label(window, bg=bg_color, fg=fg_color,  font=('Helvetica 12 bold'), text='X / Y / Z')
+accessable_text.pack()
 
 entry = tk.Entry(bg=bg_color, fg=fg_color)
 entry.pack(fill=tk.X)
